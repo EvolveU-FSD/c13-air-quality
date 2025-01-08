e3382dfe8773547d48b21fb7ef90217dfda99ae2
Tony Enerson <tony@enerson.ca>
2025-01-08 15:27:43 -0700

    Open meteo plus user location

--- src/components/AqiAtLocation.jsx
+++ src/components/AqiAtLocation.jsx
@@ -3,19 +3,34 @@
 import { useEffect, useState } from "react"
 
 export default function AqiAtLocation() {
+    const [userLocation, setUserLocation] = useState()
     const [currentAqi, setCurrentAqi] = useState({ name: 'Loading...', aqi: null})
 
     useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                console.log('Got user position', position)
+                setUserLocation(position.coords)
+            }, () => {
+                setCurrentAqi('Location Error')
+            })
+        }
+        else {
+            setCurrentAqi('No Location')
+        }
+    }, [])
+
+    useEffect(() => {
         async function loadAqi() {
-            const response = await fetch('/api/aqi?latitude=51&longitude=-114')
+            const response = await fetch(`/api/aqi?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}`)
             if (response.status !== 200) {
                 throw new Error('Fetch of AQI failed')
             }
             const aqi = await response.json()
             setCurrentAqi(aqi)
         }
-        loadAqi()
-    }, [])
+        if (userLocation) loadAqi()
+    }, [userLocation])
 
     return (<>
         <h1>{currentAqi.name}</h1>
